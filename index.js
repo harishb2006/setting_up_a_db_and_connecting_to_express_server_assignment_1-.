04954a63b8a2dc55494c7b7c25218b988e746e82
harishb2006 <imharishba@gmail.com>
2025-02-08 09:52:58 +0530

    Revert "db connected"
    This reverts commit 82861347364b52f81213c018384cf55c0d49c1ab.

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-require('dotenv').config()
-const mongoose=require('mongoose');
-
 const express = require('express');
 const { resolve } = require('path');
-
-
-mongoose.connect(process.env.DB_URL)
-  .then(()=>{
-    console.log('Mongo db succesfully connected')
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
 
 const app = express();
 const port = 3010;
@@ -21,11 +9,6 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
