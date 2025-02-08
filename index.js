82861347364b52f81213c018384cf55c0d49c1ab
harishb2006 <imharishba@gmail.com>
2025-02-08 09:34:54 +0530

    db connected

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+require('dotenv').config()
+const mongoose=require('mongoose');
+
 const express = require('express');
 const { resolve } = require('path');
+
+
+mongoose.connect(process.env.DB_URL)
+  .then(()=>{
+    console.log('Mongo db succesfully connected')
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
 
 const app = express();
 const port = 3010;
@@ -9,6 +21,11 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
